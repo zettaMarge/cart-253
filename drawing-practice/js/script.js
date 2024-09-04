0ed1385485e2e4b88f450fb555f86414b60743cd
Marge <72757961+zettaMarge@users.noreply.github.com>
2024-09-04 12:48:43 -0400

    edit js drawing-practice demo;

--- drawing-practice/js/script.js
+++ drawing-practice/js/script.js
@@ -1,24 +1,50 @@
 /**
- * Title of Project
- * Author Name
+ * Prunsel
+ * Marjorie Dudemaine
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A representation of a giant eye on a canvas using the p5 tool
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * initializes a canvas on the page
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws a big eye on the canvas
 */
 function draw() {
+    background(150, 150, 150);
 
+    //white base for the eye
+    push();
+    fill(255);
+    stroke(255, 255, 255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    //eye's dark iris
+    push();
+    fill(25);
+    noStroke();
+    ellipse(320, 320, 300, 300);
+    pop();
+
+    //eye's pupil
+    push();
+    fill(0);
+    stroke(35, 35, 35);
+    ellipse(320, 320, 75, 75);
+    pop();
+
+    //eye's shine
+    push();
+    fill(255);
+    noStroke();
+    ellipse(225, 275, 50, 50);
+    pop();
 }
