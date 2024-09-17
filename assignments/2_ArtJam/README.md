b339b8d45315d1b331f9e49fac73f71303378fd9
Marge <72757961+zettaMarge@users.noreply.github.com>
2024-09-17 14:28:03 -0400

    add ArtJam assignment base program;

---
+++ assignments/2_ArtJam/README.md
@@ -0,0 +1,8 @@
+# Assignment 2: Art Jam
+## TITLE
+
+### Description
+desc
+
+### Credits
+This project uses [p5.js](https://p5js.org).
---
+++ assignments/2_ArtJam/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Title
+ * Marjorie Dudemaine
+ * 
+ * desc
+ */
+
+"use strict";
+
+/**
+ * Create the canvas
+ */
+function setup() {
+
+}
+
+/**
+ * Draw on the canvas
+ */
+function draw() {
+
+}
