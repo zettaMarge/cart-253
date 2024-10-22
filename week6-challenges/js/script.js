513d163059cf9f7def22b96d080d5917b51a810c
Marge <marjoriedudemaine@hotmail.com>
2024-10-22 12:53:23 -0400

    edit week 6 challenges;

--- week6-challenges/js/script.js
+++ week6-challenges/js/script.js
@@ -17,7 +17,7 @@ const ball = {
     height: 10,
     velocity: {
         x: 0,
-        y: 0
+        y: 2
     }
 };
 
@@ -28,6 +28,8 @@ const paddle = {
     width: 80,
     height: 10
 };
+
+const gravity = 0.1;
 
 /**
  * Create the canvas
@@ -48,26 +50,79 @@ function draw() {
 
     handleBounce(ball, paddle);
 
-    drawPaddle(paddle);
-    drawBall(ball);
+    drawBlock(paddle);
+    drawBlock(ball);
 }
 
 /**
- * Moves the paddle
+ * Challenge 2: Moves the paddle
  */
 function movePaddle(paddle) {
-
+    paddle.x = constrain(mouseX, 0, width);
 }
 
 /**
- * Moves the ball
+ * Challenge 3: Moves the ball
+ * Challenge 5: Gravity
  */
 function moveBall(ball) {
+    ball.velocity.y += gravity;
+    ball.y = constrain(ball.y + ball.velocity.y, 0, height);
 
+    if (ball.y < height) {
+        // Move the ball left/right if it's in the air
+        ball.x = constrain(ball.x + ball.velocity.x, 0, width);
+    }
 }
 
+/**
+ * Challenge 4: Handle the ball bounce
+ */
 function handleBounce(ball, paddle) {
+    if (isRectPointOverlap(paddle.x, paddle.y, paddle.width, paddle.height, ball.x, ball.y)) {
+        // Bounce off the paddle
+        ball.velocity.y = ball.velocity.y * -1;
 
+        if (ball.x > paddle.x - paddle.width/2 && ball.x < paddle.x) {
+            // If the ball touches the left side of the paddle, bounce left
+            ball.velocity.x = -1.5;
+        }
+        else {
+            // If the ball touches the right side of the paddle, bounce right
+            ball.velocity.x = 1.5;
+        }
+    }
+    else if (ball.y <= 0) {
+        // Bounce off the ceiling
+        ball.velocity.y = ball.velocity.y * -1;
+    }
+
+    if (ball.x <= 0 || ball.x >= width) {
+        // Bounce off the walls
+        ball.velocity.x = ball.velocity.x * -1;
+    }
+}
+
+/**
+ * Returns if a point overlaps a rectangle, assuming rectMode is set to CENTER
+ * @param {Number} xR x coordinate of the rectangle
+ * @param {Number} yR y coordinate of the rectangle
+ * @param {Number} wR width of the rectangle
+ * @param {Number} hR height of the rectangle
+ * @param {Number} xP x coordinate of the point
+ * @param {Number} yP y coordinate of the point
+ * @returns 
+ */
+function isRectPointOverlap(xR, yR, wR, hR, xP, yP) {
+    // These give us the bounds of the rectangle's width and height
+    let xMin = xR - wR/2;
+    let xMax = xR + wR/2;
+    let yMin = yR - hR/2;
+    let yMax = yR + hR/2;
+
+    // Is the point's x coordinate within the bounds of the rectangle's width
+    // and is the y coordinate within the bounds of the rectangle's height?
+    return (xP >= xMin && xP <= xMax && yP >= yMin && yP <= yMax);
 }
 
 /**
@@ -91,5 +146,17 @@ function drawBall(ball) {
     noStroke();
     fill("pink");
     rect(ball.x, ball.y, ball.width, ball.height);
+    pop();
+}
+
+/**
+ * Challenge 1: function that can draw both elements
+ */
+function drawBlock(block) {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill("pink");
+    rect(block.x, block.y, block.width, block.height);
     pop();
 }
