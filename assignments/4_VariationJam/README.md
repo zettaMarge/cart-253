12a2536ba38a911ab6e906f505213b2699dbc609
Marge <marjoriedudemaine@hotmail.com>
2024-11-05 14:31:01 -0500

    VariationJam => add base;

---
+++ assignments/4_VariationJam/README.md
@@ -0,0 +1,12 @@
+# Assignment 4: Variation Jam
+## TITLE
+
+### Description
+TODO desc
+
+### Credits
+This project uses:
+- [p5.js](https://p5js.org)
+
+### Attribution
+TODO
---
+++ assignments/4_VariationJam/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * TODO title
+ * Marjorie Dudemaine
+ * 
+ * TODO desc
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws on the canvas
+*/
+function draw() {
+
+}
